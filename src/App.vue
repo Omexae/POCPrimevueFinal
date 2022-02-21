a4bf33c07e6cf88054808ec0a580b0904b2fc29a
Omexae <antoinelejudoka@aol.com>
2022-02-21 14:31:33 +0100

    oui

--- src/App.vue
+++ src/App.vue
@@ -1,212 +1,40 @@
 <template>
-  <div class="p-d-block" id="Welcometext">
-    <!--<InputText v-model="text"/>
-    <Button label="Greet" icon="pi pi-user" @click="greet" id="btn"></Button>
-    {{text}}-->
-    <Toast />
-    Click on the header corresponding to the KPI you want to get informations about !
-  </div>
-  <TabView scrollable>
-    <TabPanel header="Sample">
-      <div v-if="hidden" id="charts">
-        <div class="p-d-flex" >
-          <Chart type="bar" :data="basicData" id="chart" :options="options" :width="375" :height="250" class="p-d-flex"/>
-          <Chart ref="primeChart" type="pie" :data="chartData" id="chart" :width="375" :height="250" :options="options" class="p-d-flex"/>
-          <Chart type="line" :data="LineData" id="chart" :options="options" :width="375" :height="250" class="p-d-flex"/>
-          <div class="p-d-block">
-            <Button label ="TestAnotherSet" v-bind:badge="RSEdata" icon="pi pi-chart-bar" id="RseBtn" class="p-d-block" @click="changeInterface"></Button>
-            <Button label ="Test1" v-bind:badge="RSEdata1" icon="pi pi-percentage" id="RseBtn" class="p-d-block"></Button>
-        </div>
-        </div>
-        <div class="p-d-flex">
-          <Chart type="polarArea" :data="PolarData" id="chart" :width="375" :height="250" :options="options" class="p-d-inline"/>
-          <Chart type="radar" :data="RadarData" id="chart" :width="375" :height="250" :options="options" class="p-d-inline"/>
-          <Chart type="bar" :data="BarData" id="chart" :width="375" :height="250" :options="options" class="p-d-inline"/>
-          <div class="p-d-block">
-            <Button label ="Test2" v-bind:badge="RSEdata2" icon="pi pi-key" id="RseBtn" class="p-d-block" ></Button>
-            <Button label ="TestingDataFromMagisoft" v-bind:badge="RSEdata3" icon="pi pi-heart" id="RseBtn" class="p-d-block" @click="addData"></Button>
-          </div>
-        </div>
-      </div>
-      <div v-if="!hidden">
-        Bring up new interface here with charts and all
-        <Button label ="TestAnotherSet" v-bind:badge="RSEdata" icon="pi pi-chart-bar" id="RseBtn" class="p-d-block" @click="changeInterfaceBack"></Button>
-      </div>
-      <img id="logo" src="./assets/okteologo.png"/>
-      <img id="logo" src="./assets/magisoftlogo.jpg"/>
-    </TabPanel>
-    <TabPanel header="Espace de démonstration" >
-      Ceci est un espace qui présente différents components de PrimeVue. Il en existe d'autres, mais ici sont sélectionnés les plus susceptibles d'être utiles.
-      <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" class="p-d-flex"/>
-      <Knob v-model="value" />
-      <Knob v-model="valueR" readonly="true"/>
-      <Knob v-model="value" valueColor="Green" rangeColor="Purple"/>
-      <Listbox v-model="selectedCity" :options="cities" optionLabel="name" />
-      <SelectButton v-model="selectedCity" :options="cities" optionLabel="code" />
-    </TabPanel>
-      <TabPanel header="Affichage de côté">
-      <div class="p-d-block" :style="{ backgroundImage: `url(${backgimg})`}" style="width: 79%; height: 650px">
-        <ScrollPanel style="width: 15%; height: 700px; paddingTop: 60px; paddingBottom: 150px" class="custom">
-          <Chart type="bar" :data="basicData" id="chart" :options="options" :width="140" :height="250" @click="addData" class="p-d-flex"/>
-          <Chart ref="primeChart" type="pie" :data="chartData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Chart type="line" :data="LineData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Chart type="polarArea" :data="PolarData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Chart type="radar" :data="RadarData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Chart type="bar" :data="BarData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Button icon="pi pi-plus" @click="visibleFull = true"  class="p-d-block"/>
+    <div>
+        <ScrollPanel class="custom">
+          <Chart type="bar" :data="basicData" id="chart" :options="options" @click="addData"/>
+          <Chart ref="primeChart" type="pie" :data="chartData" id="chart" :options="options" />
+          <Chart type="line" :data="LineData" id="chart" :options="options" />
+          <Chart type="polarArea" :data="PolarData" id="chart" :options="options" />
+          <Chart type="radar" :data="RadarData" id="chart" :options="options" />
+          <Chart type="bar" :data="BarData" id="chart" :options="options" />
+          <Button icon="pi pi-minus" @click="tryMagisoft"  />
+          <Button class="pi pi-replay" @click="getJsonData" />
         </ScrollPanel>
-        <Sidebar v-model:visible="visibleFull" position="full">
-          <div class="p-d-flex" v-if="SideBarHidden" id="charts" >
-            <Chart type="bar" :data="basicData" id="chart" :options="options" :width="375" :height="250" class="p-d-flex"/>
-            <Chart ref="primeChart" type="pie" :data="chartData" id="chart" :width="375" :height="250" :options="options" class="p-d-flex"/>
-            <Chart type="line" :data="LineData" id="chart" :width="375" :height="250" :options="options" class="p-d-flex"/>
-            <div class="p-d-block">
-              <Button label ="TestAnotherSet" v-bind:badge="RSEdata" icon="pi pi-chart-bar" id="RseBtn" class="p-d-block" @click="changeInterfaceSidebar"></Button>
-              <Button label ="Test1" v-bind:badge="RSEdata1" icon="pi pi-percentage" id="RseBtn" class="p-d-block"></Button>
-            </div>
-          </div>
-          <div class="p-d-flex" v-if="SideBarHidden" id="charts">
-            <Chart type="polarArea" :data="PolarData" id="chart" :width="375" :height="250" :options="options" class="p-d-inline"/>
-            <Chart type="radar" :data="RadarData" id="chart" :width="375" :height="250" :options="options" class="p-d-inline"/>
-            <Chart type="bar" :data="BarData" id="chart" :width="375" :height="250" :options="options" class="p-d-inline"/>
-            <div class="p-d-block">
-              <Button label ="Test2" v-bind:badge="RSEdata2" icon="pi pi-key" id="RseBtn" class="p-d-block" ></Button>
-              <Button label ="TestingDataFromMagisoft" v-bind:badge="RSEdata3" icon="pi pi-heart" id="RseBtn" class="p-d-block" @click="addData"></Button>
-            </div>
-          </div>
-          <div v-if="!SideBarHidden">
-              <Button label ="TestAnotherSet" v-bind:badge="RSEdata" icon="pi pi-chart-bar" id="RseBtn" class="p-d-block" @click="changeInterfaceBackSidebar"></Button>
-          </div>
-        </Sidebar>
       </div>
-    </TabPanel>
-    <TabPanel header="Test affichage Magisoft">
-    <div class="p-d-block" style="width: 79%; height: 650px">
-        <ScrollPanel style="width: 15%; height: 700px; paddingTop: 60px; paddingBottom: 150px" class="custom">
-          <Chart type="bar" :data="basicData" id="chart" :options="options" :width="140" :height="250" @click="addData" class="p-d-flex"/>
-          <Chart ref="primeChart" type="pie" :data="chartData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Chart type="line" :data="LineData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Chart type="polarArea" :data="PolarData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Chart type="radar" :data="RadarData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Chart type="bar" :data="BarData" id="chart" :width="140" :height="250" :options="options" class="p-d-flex"/>
-          <Button icon="pi pi-plus" @click="visibleFull = true"  class="p-d-block"/>
-        </ScrollPanel>
-        <Sidebar v-model:visible="visibleFull" position="full">
-          <div class="p-d-flex" v-if="SideBarHidden" id="charts" >
-            <Chart type="bar" :data="basicData" id="chart" :options="options" :width="375" :height="250" class="p-d-flex"/>
-            <Chart ref="primeChart" type="pie" :data="chartData" id="chart" :width="375" :height="250" :options="options" class="p-d-flex"/>
-            <Chart type="line" :data="LineData" id="chart" :width="375" :height="250" :options="options" class="p-d-flex"/>
-            <div class="p-d-block">
-              <Button label ="TestAnotherSet" v-bind:badge="RSEdata" icon="pi pi-chart-bar" id="RseBtn" class="p-d-block" @click="changeInterfaceSidebar"></Button>
-              <Button label ="Test1" v-bind:badge="RSEdata1" icon="pi pi-percentage" id="RseBtn" class="p-d-block"></Button>
-            </div>
-          </div>
-          <div class="p-d-flex" v-if="SideBarHidden" id="charts">
-            <Chart type="polarArea" :data="PolarData" id="chart" :width="375" :height="250" :options="options" class="p-d-inline"/>
-            <Chart type="radar" :data="RadarData" id="chart" :width="375" :height="250" :options="options" class="p-d-inline"/>
-            <Chart type="bar" :data="BarData" id="chart" :width="375" :height="250" :options="options" class="p-d-inline"/>
-            <div class="p-d-block">
-              <Button label ="Test2" v-bind:badge="RSEdata2" icon="pi pi-key" id="RseBtn" class="p-d-block" ></Button>
-              <Button label ="TestingDataFromMagisoft" v-bind:badge="RSEdata3" icon="pi pi-heart" id="RseBtn" class="p-d-block" @click="addData"></Button>
-            </div>
-          </div>
-          <div v-if="!SideBarHidden">
-              <Button label ="TestAnotherSet" v-bind:badge="RSEdata" icon="pi pi-chart-bar" id="RseBtn" class="p-d-block" @click="changeInterfaceBackSidebar"></Button>
-          </div>
-        </Sidebar>
-      </div>
-    </TabPanel>
-    <TabPanel header="KPI3">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI4">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI5">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI6">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI7">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI8">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI9">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI10">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI11">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI12">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI13">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI14">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI15">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI16">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI17">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI18">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI19">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI20">
-      Another tab that can be filled with graphs
-    </TabPanel>
-    <TabPanel header="KPI21">
-      Another tab that can be filled with graphs
-    </TabPanel>
-  </TabView>
-  
 </template>
 
 <script>
 
 import { ref } from 'vue'
-import backgimg from './assets/magisoft_rognee.png'
+import DataJson from "./data.json";
+import testdataJson from "./testdata.json"
+import testdataMGJson from "./testdataMG.json"
+
 
 export default {
   data(){
     return{
-      backgimg,
-      visibleFull: false,
+      msg: "5448",
       value:0,
-      SideBarHidden: true,
       valueR:50,
-      selectedCity: null,
-		  cities: [
-			  {name: 'New York', code: 'NY'},
-			  {name: 'Rome', code: 'RM'},
-			  {name: 'London', code: 'LDN'},
-			  {name: 'Istanbul', code: 'IST'},
-			  {name: 'Paris', code: 'PRS'}
-		  ],
+      JData : [150,200,300],
+      TestDataMG: testdataMGJson,
+      i:0,
       primeChart: ref(),
-      text: null,
-      RSEdata: "15",
-      RSEdata1: "5",
-      RSEdata2: "25",
-      RSEdata3: "184951",
       hidden: true,
-      TestData: [65, 59, 80, 81, 56, 55, 2],
+      TestData: testdataJson,
       data1: 65,
+      JsonData : DataJson,
       dataCollection: null,
       basicData: {
 				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -227,7 +55,7 @@ export default {
                 labels: ['A','B','C'],
                 datasets: [
                     {
-                        data: [300, 50, 100],
+                        data: DataJson,
                         backgroundColor: ["#42A5F5","#66BB6A","#FFA726"],
                         hoverBackgroundColor: ["#64B5F6","#81C784","#FFB74D"]
                     }
@@ -308,8 +136,10 @@ export default {
                 }]
             },
       options:{
-        responsive: false,
+        responsive: true,
         hoverMode:'index',
+        maintainAspectRatio : true,
+        autoPadding : false,
       }
       
     }
@@ -334,11 +164,11 @@ export default {
           {
             label:'Deuxième dataset',
             backgroundColor: 'purple',
-            data: [65, 59, 80, 81, 56, 55, 150],
+            data: this.TestDataMG,
           },
         ],
       };
-      this.$toast.add({severity :'info', summary:'On click', detail: 'Data from first chart changed dynamically, can use with data recorded from magisoft', life: 10000});
+      CefSharp.PostMessage("Bar chart changed")
     },
     changeInterfaceBack(){
       this.hidden = true;
@@ -351,7 +181,35 @@ export default {
     changeInterfaceBackSidebar(){
       this.SideBarHidden = true;
       this.$toast.add({severity :'info', summary:'On click', detail: 'Interface changed', life: 3000});
-    }
+    },
+    tryMagisoft(){
+      CefSharp.PostMessage(this.msg.toString());
+    },
+    tryMagisoftWhile(){
+      this.i=0;
+      while(this.i < this.msg.length){
+        CefSharp.PostMessage(this.msg.charAt(this.i));
+        //console.info(this.msg.charAt(this.i))
+        this.i++;
+      }
+    },
+    getJsonData(){
+      /*fetch("data.json")
+        .then(response => response.json())
+        .then(data => (this.JsonData = data));*/
+      console.log(this.JsonData);
+      this.chartData = {
+                        labels: ['A','B','C'],
+                datasets: [
+                    {
+                        data: this.JsonData, 
+                        backgroundColor: ["#42A5F5","#66BB6A","#FFA726"],
+                        hoverBackgroundColor: ["#64B5F6","#81C784","#FFB74D"]
+                    }
+                ],
+            };
+            CefSharp.PostMessage("Circle chart changed");
+      }
   },
 }
 </script>
@@ -362,7 +220,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #000000;
-  margin-top: 15px;
   background-color:whitesmoke ;
   text-align: center;
 /*Ligne de test qui permet de vérifier si la sidebar se scale avec la taille de l'app ou la taille de l'écran
@@ -374,30 +231,22 @@ export default {
   background-color: rgb(129, 6, 129);
   color: rgb(9, 170, 9);
 }
-#chart{
-  padding-right: 50px;
-}
 #RseBtn{
   background-color: rgb(129, 6, 129);
   color: rgb(9, 170, 9);
-  width: 200px;
-  height: 100px;
+  width: 2rem;
+  height: 1rem;
 }
 #charts{
-  text-align:right;
+  text-align:center;
   background-color: blanchedalmond;
 }
-#Welcometext{
-  font-family: 'Times New Roman', Times, serif;
-  font-size: 25px;
-  color: purple;
-  background-color: rgb(9, 170, 9);
-  font-weight: bold;
+#chart{
+  padding-bottom: 10rem;
 }
-#logo{
-  width: 300px;
-  max-width: 100%;
-  height: auto;
+#charts.p-chart{
+  max-width: 100vw;
+  width: 100vw;
 }
 .custom .p-scrollpanel-wrapper {
     border-right: 9px solid #f4f4f4;
@@ -409,5 +258,11 @@ export default {
 }
 .custom .p-scrollpanel-bar:hover {
     background-color: #135ba1;
+}
+.custom {
+  align-content: center;
+  text-align: center;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
